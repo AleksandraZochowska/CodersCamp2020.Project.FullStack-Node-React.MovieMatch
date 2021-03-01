37e09e22504c353e3690445cf8ff5838a35806f4
Aleksandra Zochowska <a.j.zochowska@gmail.com>
2021-03-01 17:23:05 +0100

    added login, authorization, connecting & disconnecting from db

--- app/models/users/UserModel.js
+++ app/models/users/UserModel.js
@@ -1,11 +1,59 @@
 const UserController = require("../../controllers/users/UserController");
+const Model = require("../Model");
+const userSchema = require("./userSchema");
+const hashSchema = require("./hashSchema");
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-class UserModel {
-    //get user po konkretnym id z database
-    //get users po zadanych warunkach z database
-    //autoryzacja
-    //wyszukiwanie w bazie po id, emailu, itd.
-    //updateowanie usera
+class UserModel extends Model {
+
+    constructor() {
+        super()
+        this.User = mongoose.model("User", userSchema);
+        this.Hash = mongoose.model("Hash", hashSchema);
+        this.mongoURL = process.env.MONGO_URL;
+        this.mongoDB = process.env.MONGO_DB;
+    }
+    
+    findByEmail(email) {
+
+        return new Promise((resolve, reject) => {
+
+            this.connectToDB();
+
+            this.User.findOne({email: email}, (err, user) => {
+                this.disconnectFromDB();
+                if (err) reject(err);
+                resolve(user);
+            });
+
+        });
+    }
+
+    authorize(user, password) {
+
+        return new Promise(async (resolve, reject) => {
+
+            await this.connectToDB();
+
+            this.Hash.findOne({userId: user._id}, (err, hash) => {
+                this.disconnectFromDB();
+                if (err) reject(err);
+                console.log(hash.hash)
+                bcrypt.compare(`${password}`, hash.hash, (authError, result) => {
+                    if (authError) reject(authError);
+                    if (result) {
+                        const token = jwt.sign({userId: user._id}, `${process.env.SECRET_TOKEN}`, { expiresIn: "1h" });
+                        resolve(token);
+                    }
+                    resolve(false);
+                });
+
+            });
+
+        });
+    }
 }
 
 module.exports = UserModel;
--- app/models/users/hashSchema.js
+++ app/models/users/hashSchema.js
@@ -1,10 +1,12 @@
+const mongoose = require('mongoose');
+
 const hashSchema = mongoose.Schema({
 
     _id: mongoose.Schema.Types.ObjectId,
     userId: mongoose.Schema.Types.ObjectId,
     hash: String,
     updatedAt: Date,
-        
+
 });
 
 module.exports = hashSchema;

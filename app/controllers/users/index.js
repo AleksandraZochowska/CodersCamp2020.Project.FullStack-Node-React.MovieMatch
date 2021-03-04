645a112a6cde50c8874a0446b3bf9d5c5a0945c8
AnnZak <zak.anna.94@gmail.com>
2021-03-04 22:42:34 +0100

    Update index.js

--- app/controllers/users/index.js
+++ app/controllers/users/index.js
@@ -3,8 +3,6 @@ const UserController = require("./UserController");
 // POST:
 module.exports.login = (req, res) => { new UserController(req, res).login() };
 module.exports.register =  (req, res) => { new UserController(req, res).register() };
-
-// PUT:
 module.exports.forgotPassword = (req, res) => { new UserController(req, res).forgotPassword() };
 
 //PATCH:

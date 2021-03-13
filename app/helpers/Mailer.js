4cd147249b3c898d1e88f0714838b021a81371ba
Aleksandra Zochowska <a.j.zochowska@gmail.com>
2021-03-13 20:07:00 +0100

    refactor: fix indentation in email templates, add development email recipient

--- app/helpers/Mailer.js
+++ app/helpers/Mailer.js
@@ -5,15 +5,16 @@ const ejs = require("ejs");
 class Mailer {
 
     constructor() {
+
         this.transporter = nodemailer.createTransport({
             host: 'smtp.gmail.com',
             port: 587,
             secure: false,
-            requireTLS: true,
             auth: {
                 user: process.env.DOMAIN_EMAIL,
                 pass: process.env.DOMAIN_EMAIL_PASS
             },
+            requireTLS: true,
             tls: {
                 rejectUnauthorized: false
             },
@@ -24,24 +25,23 @@ class Mailer {
 
         return new Promise((resolve, reject) => {
 
-            ejs.renderFile(__dirname + `/templates/${template}.ejs`, { name: name, token: token }, (err, renderedFile) => {
+            ejs.renderFile(__dirname + `/templates/${template}.ejs`, { name: name, token: token }, (error, renderedFile) => {
 
-                if(err) reject(error);
+                if(error) reject(error);
                 if(!renderedFile) resolve(false);
 
-                let mailOptions = {
+                const mailOptions = {
                     from: `"MovieMatch" <${process.env.DOMAIN_EMAIL}>`,
-                    to: `${process.env.DOMAIN_EMAIL}`,
-                    // to: `${recipient}`,
+                    to: `${process.env.DEBUG_RECIPIENT || recipient}`,
                     subject: `MovieMatch | ${template === "resetpw" ? "Reset password"
                         : template === "register" ? "Confirm Registration"
                         : "Newsletter"}`,
                     html: renderedFile
                 };
     
-                this.transporter.sendMail(mailOptions, (error, info) => {
+                this.transporter.sendMail(mailOptions, (err, info) => {
                     
-                    if(error) reject(error);
+                    if(err) reject(err);
                     if(!info) resolve(false);
                     resolve(info);
                 });
